d7212702b398ba1fddcd1247c99c8e43cdea7acc
tonsorialdev <147235191+tonsorialdev@users.noreply.github.com>
2025-01-03 14:51:31 -0800

    Modified number generation methods to work, along with a helper method.

---
+++ src/generateNumbers.ts
@@ -0,0 +1,38 @@
+ import {join} from 'node:path'
+
+ const buildURL = (difficulty: string) => {
+     let num;
+     let max;
+     switch (difficulty) {
+         case 'easy':
+             num = 4;
+             max = 7
+             break;
+         case 'medium':
+             num = 6
+             max = 8
+             break;
+         case 'hard':
+             num = 8
+             max = 9;
+             break;
+     }
+     return `https://www.random.org/integers?num=${num}&min=0&max=${max}&col=1&base=10&format=plain`
+     // I don't think additional error handling will be needed here, as there will be buttons linked to the difficulty level and I can force string inputs there.
+ }
+
+ export const generateNumbers = async (difficulty: string): Promise<string | undefined> => {
+    const url = buildURL(difficulty);
+    try{
+        let res = await fetch(url)
+        console.log(await res.text())
+        return res.toString()
+    }
+    catch(err: unknown){
+        console.log(`Error: ${err}`)
+    }
+ }
+
+
+
+// num is the only differentiating factor, so just make it a variable according to the difficulty.
