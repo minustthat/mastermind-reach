22c3138b037d5ee6285293817b097d5b8825748a
tonsorialdev <147235191+tonsorialdev@users.noreply.github.com>
2025-01-04 19:30:26 -0800

    Created methods to register and login players, as well as a collection to keep registered players in.

--- src/gameComponents/player.ts
+++ src/gameComponents/player.ts
@@ -1,29 +1,36 @@
-import {EventEmitter} from "node:events";
-
-export class Player extends EventEmitter{
-    name: string;
-    guesses: number[] = []
-    won: boolean = false
-
-    constructor(name: string){
-        super()
-        this.name = name;
-    }
-
-    get _name(){
-        return this.name
-    }
-    get _won(){
-        return this.won
-    }
-    get _guesses(){
-        return this.guesses
-    }
-    set _won(result: boolean){
-        this.won = result
-    }
-    set _name(n: string){
-        this.name = n
-    }
-
+import bcrypt from "bcrypt"
+import SessionClient from "../database/sessionClient.ts";
+export interface Player{
+    username: string;
+    email: string;
+    password: string;
+    dateRegistered: string;
 }
+
+export const registerPlayer = async (name: string, emailAddress: string, pwd: string) => {
+    const client: SessionClient = new SessionClient()
+    try{
+        const salt: string =  bcrypt.genSaltSync(10)
+        const hashedPassword: string = await bcrypt.hash(pwd,salt)
+        const player: Player = {
+            username: name,
+            email: emailAddress,
+            password: hashedPassword,
+            dateRegistered: Date.now().toString()
+        }
+        await client.addUserToDb(player).catch(err=> console.log(`Error: ${err}`))
+        console.log(player)
+    }
+    catch(err){
+        console.log(`Error: ${err}`)
+    }
+}
+ export const loginPlayer = async (name: string, password: string)=> {
+         const client = new SessionClient()
+         await client.findUser(name,password)
+ }
+
+// encode password
+// send encoded version to db
+// use async version of hash function to avoid blocking the event loop.
+const client = new SessionClient()
