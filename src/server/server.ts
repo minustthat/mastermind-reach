d75d060bf72e94cc3369ac883f99b3a9b1640660
tonsorialdev <147235191+tonsorialdev@users.noreply.github.com>
2025-01-04 19:38:51 -0800

    Additional changes within the scope of player registration.

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,7 +3,7 @@ import cors from 'cors'
 
 import {connectToDatabase} from "../database/database.ts";
 import SessionClient from "../database/sessionClient.ts";
-import {Player} from "../gameComponents/player.ts";
+import {registerPlayer} from "../gameComponents/player.ts";
 import {SinglePlayerGameBuilder} from "../gameModes/singlePlayerGameBuilder.ts";
 import SinglePlayerGame from "../gameModes/singlePlayerGame.ts";
 import bodyParser from "body-parser";
@@ -25,55 +25,65 @@ const generateBody = (obj: Object): {} => {
 app.get('/', (req: Request, res: Response): void => {
     res.status(200).set({'Content-Type': 'text/html'}).send(`
         <form action = "/newUser" method="POST"> 
-        <label for="name"> Username: </label>
-        <input type = "text" id="name" name="name"> 
+        <label for="username"> Username: </label>
+        <input type = "text" id="username" name="username"> 
+        
+        <label for="email"> Email Address: </label>
+        <input type = "text" id="email" name="email"> 
+       
+        <label for="password"> Password : </label>
+        <input type = "password" id="password" name="password"> 
         <input type="submit">  
         </form>
     `)
 })
-app.post('/newUser', (req: Request, res: Response): void => {
+app.post('/newuser', (req: Request, res: Response): void => {
     try{
         req.accepts('text/html')
-        const name: string = req.body.name
-        const player = new Player(name)
-        res.status(200).send(`${name}`)
+        res.status(201)
+        req.setTimeout(5000, () => {
+            console.log('Request timed out');
+            res.status(408).send('Request Timeout');
+        });
+        const name: string = req.body.username
+        const email: string = req.body.email
+        const password: string = req.body.password
+        registerPlayer(name,email,password).catch(err=> console.log(`Err: ${err}`))
         console.log(name)
-        player.emit('registered', name)
-
     } catch(err: unknown){
         console.log(err)
     }
 })
 
-app.get('/newgame', async (req: Request, res: Response) => {
-    res.send(`
-        <form action = "/gameconfig" method="POST"> 
-        <label for="difficultyLevel"> Username: </label>
-        <input type = "text" id="difficultyLevel" name="difficultyLevel"> 
-        <input type="submit">  
-        </form>
-    `)
-})
-app.post('/gameconfig', async (req: Request, res: Response) => {
-
-    try
-    {
-        req.accepts('text/json')
-        res.status(201)
-        const game: SinglePlayerGame = new SinglePlayerGameBuilder()
-            .withPlayer(new Player(req.body.Player))
-            .withDifficulty(req.body.difficultyLevel)
-            .withHintsEnabled(req.body.difficultyLevel != 'hard')
-            // hints only enabled for hard difficulty games.
-            .withHints(req.body.setHints == true ? ['none'] : ['none'])
-            .build()
-        res.send(game)
-    }
-    catch (err: unknown)
-    {
-        console.log(err)
-    }
-})
+// app.get('/newgame', async (req: Request, res: Response) => {
+//     res.send(`
+//         <form action = "/gameconfig" method="POST">
+//         <label for="difficultyLevel"> Username: </label>
+//         <input type = "text" id="difficultyLevel" name="difficultyLevel">
+//         <input type="submit">
+//         </form>
+//     `)
+// })
+// app.post('/gameconfig', async (req: Request, res: Response) => {
+//
+//     try
+//     {
+//         req.accepts('text/json')
+//         res.status(201)
+//         const game: SinglePlayerGame = new SinglePlayerGameBuilder()
+//             .withPlayer(new Player(req.body.Player))
+//             .withDifficulty(req.body.difficultyLevel)
+//             .withHintsEnabled(req.body.difficultyLevel != 'hard')
+//             // hints only enabled for hard difficulty games.
+//             .withHints(req.body.setHints == true ? ['none'] : ['none'])
+//             .build()
+//         res.send(game)
+//     }
+//     catch (err: unknown)
+//     {
+//         console.log(err)
+//     }
+// })
 app.post('/play', (req: Request, res: Response) => {
 })
 
