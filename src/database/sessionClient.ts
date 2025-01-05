17bf066728ae9f5a449d04cb1527b500db8ff640
tonsorialdev <147235191+tonsorialdev@users.noreply.github.com>
2025-01-04 19:38:51 -0800

    Additional changes within the scope of player registration.

--- src/database/sessionClient.ts
+++ src/database/sessionClient.ts
@@ -1,13 +1,42 @@
-import {collection} from "./database.ts";
+import {sessionCollection,userCollection} from "./database.ts";
+import {Player} from "../gameComponents/player.ts";
+import bcrypt from "bcrypt";
 
 export default class SessionClient{
-    addToDb = async (obj: Object) => {
+    addSessionToDb = async (obj: Object) => {
         try {
-            await collection.insertOne(obj)
+            await sessionCollection.insertOne(obj)
             return `Object inserted: ${JSON.stringify(obj)}`
         }
         catch(err: unknown){
             return err
+        }
+    }
+
+    addUserToDb = async (obj: Player) => {
+        try{
+            await userCollection.insertOne(obj)
+            return `Object inserted: ${JSON.stringify(obj)}`
+        }
+        catch(err){
+            console.log(`Error adding User to DB: ${err}`)
+        }
+    }
+
+    findUser = async(username: string, pwd: string) => {
+        try{
+            const user = await userCollection.findOne({username: username})
+            // @ts-ignore
+            const password = await bcrypt.compare(pwd,user.password)
+            if(!user || !password){
+                console.log('Re-enter information.')
+            }
+            return user
+            //compare the hash of the stored version, and current version.
+                // @ts-ignore
+        }
+        catch(err){
+            console.log(`Incorrect information: ${err}`)
         }
     }
 }
