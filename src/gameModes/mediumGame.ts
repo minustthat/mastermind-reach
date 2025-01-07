0c449c7cb29ce0f2fe68adaa26fa05b656dd9fbe
tonsorialdev <147235191+tonsorialdev@users.noreply.github.com>
2025-01-06 18:21:00 -0800

    Guess methods, the beginning of testing for the game logic, and associated changes.'

---
+++ src/gameModes/mediumGame.ts
@@ -0,0 +1,13 @@
+import SinglePlayerGameConfiguration from "./gameConfiguration.ts";
+import {Player} from "../gameComponents/player.ts";
+
+class MediumGame extends SinglePlayerGameConfiguration{
+    override player: Player
+    override difficulty: string = 'easy';
+    override hints: string[] = []
+    override hintsEnabled: boolean = true
+    constructor(player:Player){
+        super(player)
+        this.player = player
+    }
+}
