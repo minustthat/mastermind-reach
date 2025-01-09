b44c2648414bbe2f13578b160868fea21785b154
tonsorialdev <147235191+tonsorialdev@users.noreply.github.com>
2025-01-09 01:53:09 -0800

    redoing commit.

--- src/game-components/generateNumbers.ts
+++ src/game-components/generateNumbers.ts
@@ -18,7 +18,6 @@ const buildURL = (difficulty: string) => {
     return `https://www.random.org/integers?num=${num}&min=0&max=${max}&col=1&base=10&format=plain`
     // I don't think additional error handling will be needed here, as there will be buttons linked to the difficulty level and I can force string inputs there.
 }
-
 export const generateNumbers = async (difficulty: string): Promise<string | undefined> => {
     const url = buildURL(difficulty);
     try {
@@ -28,6 +27,4 @@ export const generateNumbers = async (difficulty: string): Promise<string | unde
         console.log(`Error: ${err}`)
     }
 }
-
-
 // num is the only differentiating factor, so just make it a variable according to the difficulty.
